e18b1ac57210e7ba24620ee8c7fcfd474a3c6c72
Billy <billybdev@gmail.com>
2022-07-24 19:43:55 -0700

    add connecting lines using Xarrow

--- src/UI/NodeList.js
+++ src/UI/NodeList.js
@@ -1,44 +1,84 @@
-import { MenuItem } from '@szhsin/react-menu';
-import feather from 'feather-icons';
-import SettingsButton from './SettingsButton';
+import { MenuItem } from "@szhsin/react-menu";
+import feather from "feather-icons";
+import SettingsButton from "./SettingsButton";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 
-const icon = feather.icons["box"].toSvg({"stroke-width": 1, "width": "24px"});
+const icon = feather.icons["folder"].toSvg({
+  "stroke-width": 2,
+  width: "24px",
+});
 // const icon = ""
 /** List of nodeRefs to be displayed in UI */
-export default function NodeList({setPath, setSelectedPage, nodeRefs, addNode, deleteNode}){
+export default function NodeList({
+  setPath,
+  setSelectedPage,
+  nodeRefs,
+  addNode,
+  deleteNode,
+}) {
+  const nodeStyles =
+    "group flex w-5/6 ml-auto my-3 py-4 px-6 rounded-2xl bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600";
 
-    const nodeStyles = "group flex w-3/4 m-auto my-3 py-5 px-6 rounded-2xl bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600";
-
-    const nodes = nodeRefs.map((item, index) => {
-        if (item.path[0] === null){
-            return (
-                <div className={nodeStyles} key={index}>
-                    <input 
-                     className="w-full m-auto border-2 border-zinc-500 rounded-xl dark:bg-zinc-600 py-2 px-5"
-                     type="text" 
-                     placeholder="New Node"
-                     autoFocus 
-                     onBlur={(e) => {addNode(e.target.value)}}   
-                     onKeyPress={(e) => {
-                         //@ts-ignore
-                         if (e.key === "Enter") addNode(e.target.value)
-                     }} 
-                     />
-                </div>
-            )
-        } else return (
-            <div 
-             onClick={() => {
+  const nodes = nodeRefs.map((item, index) => {
+    if (item.path[0] === null) {
+      return (
+        <div className="w-3/4 m-auto">
+          <div className={nodeStyles} key={index}>
+            <input
+              className="w-full m-auto border-2 border-zinc-500 rounded-xl dark:bg-zinc-600 py-2 px-5"
+              type="text"
+              placeholder="New Node"
+              autoFocus
+              onBlur={(e) => {
+                addNode(e.target.value);
+              }}
+              onKeyPress={(e) => {
+                //@ts-ignore
+                if (e.key === "Enter") addNode(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      );
+    } else
+      return (
+        <div className="w-3/4 m-auto">
+          <Xwrapper>
+            <div
+              onClick={() => {
                 setSelectedPage(null);
                 setPath(item.path);
-             }}
-             className={nodeStyles} key={index}><span className="mr-2" dangerouslySetInnerHTML={{__html: icon}}/>{item.title}
-             <SettingsButton>
-                 <MenuItem onClick={() => deleteNode(item.title)}>Delete</MenuItem>
-             </SettingsButton>
+              }}
+              className={nodeStyles}
+              id={"noderef-" + index}
+              key={index}
+            >
+              <span
+                className="mr-2"
+                dangerouslySetInnerHTML={{ __html: icon }}
+              />
+              {item.title}
+              <SettingsButton>
+                <MenuItem onClick={() => deleteNode(item.title)}>
+                  Delete
+                </MenuItem>
+              </SettingsButton>
             </div>
-        )
-    })
+            <Xarrow
+              start="back-beginning"
+              end={"noderef-" + index}
+              showHead={false}
+              startAnchor="bottom"
+              endAnchor="left"
+              color="#eee"
+              curveness={1.05}
+              strokeWidth={1}
+              path="grid"
+            />
+          </Xwrapper>
+        </div>
+      );
+  });
 
-    return <div className='pt-5 pb-3'>{nodes}</div>
+  return <div className="pt-3 pb-3">{nodes}</div>;
 }
--- src/UI/Sidebar.js
+++ src/UI/Sidebar.js
@@ -1,130 +1,189 @@
-import { useState, useEffect } from 'react';
-import NodeRef from '../Classes/NodeRef.js';
-import PageRef from '../Classes/PageRef.js';
-import NodeList from './NodeList.js';
-import PageList from './PageList.js';
-import feather from 'feather-icons';
+import { useState, useEffect } from "react";
+import NodeRef from "../Classes/NodeRef.js";
+import PageRef from "../Classes/PageRef.js";
+import NodeList from "./NodeList.js";
+import PageList from "./PageList.js";
+import feather from "feather-icons";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 
-const backIcon = feather.icons["corner-left-up"].toSvg({"stroke-width": 2});
-const homeIcon = feather.icons["home"].toSvg({"stroke-width": 2});
+const backIcon = feather.icons["corner-left-up"].toSvg({ "stroke-width": 2 });
+const homeIcon = feather.icons["home"].toSvg({ "stroke-width": 2 });
 
+export default function Sidebar({
+  path,
+  setPath,
+  parent,
+  pageRefs,
+  setPageRefs,
+  selectedPage,
+  setSelectedPage,
+  deletePage,
+}) {
+  const [nodeRefs, setNodeRefs] = useState(parent.nodeRefGen());
 
-export default function Sidebar({ path, setPath, parent, pageRefs, setPageRefs, selectedPage, setSelectedPage, deletePage }){
-    const [nodeRefs, setNodeRefs] = useState(parent.nodeRefGen());
-    
-    // When the parent changes, update displayed nodes/pages.
-    // State setter functions are guaranteed to have a stable identity per the docs.
-    // Therefore, it's fine to include as a dependency.
-    useEffect(() => {
-        setNodeRefs(parent.nodeRefGen());
-        setPageRefs(parent.pageRefGen());
-    }, [parent, setPageRefs])
+  // When the parent changes, update displayed nodes/pages.
+  // State setter functions are guaranteed to have a stable identity per the docs.
+  // Therefore, it's fine to include as a dependency.
+  useEffect(() => {
+    setNodeRefs(parent.nodeRefGen());
+    setPageRefs(parent.pageRefGen());
+  }, [parent, setPageRefs]);
 
+  /** Adds a temporary placeholder nodeRef, pending naming and confirmation */
+  const newNode = () => {
+    const newNodeRef = new NodeRef("New Node", [null]);
+    setNodeRefs([...nodeRefs, newNodeRef]);
+  };
 
-    /** Adds a temporary placeholder nodeRef, pending naming and confirmation */
-    const newNode = () => {
-        const newNodeRef = new NodeRef("New Node", [null]);
-        setNodeRefs([...nodeRefs, newNodeRef]);
+  /**
+   * Adds new nodeRef to UI and new node to parent object
+   * @param {string} title
+   */
+  const addNode = (title) => {
+    title = title.trim();
+    if (parent.nodes.some((node) => node.title === title)) {
+      setNodeRefs(nodeRefs.slice(0, nodeRefs.length - 1));
+      alert(
+        "There's already a node with this title.  Please use a different name."
+      );
+      return;
     }
 
-    /**
-     * Adds new nodeRef to UI and new node to parent object
-     * @param {string} title 
-     */
-    const addNode = (title) => {
-        title = title.trim();
-        if (parent.nodes.some((node) => node.title === title)){
-            setNodeRefs(nodeRefs.slice(0, nodeRefs.length - 1));
-            alert("There's already a node with this title.  Please use a different name.");
-            return;
+    // autoname pages if no name is provided
+    if (title.length === 0) {
+      let latestNode = 1;
+      for (let i = 0; i < nodeRefs.length; i++) {
+        if (nodeRefs[i].title === "Node " + latestNode) {
+          latestNode++;
+          i = -1;
         }
+      }
+      title = "Node " + latestNode;
+    }
 
-        // autoname pages if no name is provided
-        if (title.length === 0) {
-            let latestNode = 1;
-            for (let i = 0; i < nodeRefs.length; i++){
-                if (nodeRefs[i].title === "Node " + latestNode) {
-                    latestNode++;
-                    i = -1;
-                }
-            }
-            title = "Node " + latestNode;
+    setNodeRefs(
+      nodeRefs
+        .slice(0, nodeRefs.length - 1)
+        .concat(new NodeRef(title, parent.path))
+    );
+    parent.createChildNode(title);
+  };
+
+  /** Adds a temporary placeholder pageRef. */
+  const newPage = () => {
+    const newPageRef = new PageRef("New Page", null);
+    setPageRefs([...pageRefs, newPageRef]);
+  };
+
+  /**
+   * Adds new pageRef to UI and new page to parent object.
+   * @param {string} title
+   */
+  const addPage = (title) => {
+    title = title.trim();
+    // check for dentical page titles in same node
+    if (parent.pages.some((page) => page.title === title)) {
+      setPageRefs(pageRefs.slice(0, pageRefs.length - 1));
+      alert(
+        "There's already a page with this title.  Please use a different name."
+      );
+      return;
+    }
+
+    // autoname pages if no name is provided
+    if (title.length === 0) {
+      let latestPage = 1;
+      for (let i = 0; i < pageRefs.length; i++) {
+        if (pageRefs[i].title === "Page " + latestPage) {
+          latestPage++;
+          i = -1;
         }
-
-
-        setNodeRefs(nodeRefs.slice(0, nodeRefs.length - 1).concat(new NodeRef(title, parent.path)));
-        parent.createChildNode(title);
-
+      }
+      title = "Page " + latestPage;
     }
 
-    /** Adds a temporary placeholder pageRef. */
-    const newPage = () => {
-        const newPageRef = new PageRef("New Page", null);
-        setPageRefs([...pageRefs, newPageRef]);
-    }
+    setPageRefs(
+      pageRefs
+        .slice(0, pageRefs.length - 1)
+        .concat(new PageRef(title, parent.path))
+    );
+    parent.createPage(title);
+    setSelectedPage(title);
+  };
 
-    /**
-     * Adds new pageRef to UI and new page to parent object.
-     * @param {string} title 
-     */
-    const addPage = (title) => {
-        title = title.trim();
-        // check for dentical page titles in same node
-        if (parent.pages.some((page) => page.title === title)){
-            setPageRefs(pageRefs.slice(0, pageRefs.length - 1));
-            alert("There's already a page with this title.  Please use a different name.");
-            return;
-        }
+  const deleteNode = (title) => {
+    parent.nodes = parent.nodes.filter((node) => node.title !== title);
+    setNodeRefs(parent.nodeRefGen());
+  };
 
-        // autoname pages if no name is provided
-        if (title.length === 0) {
-            let latestPage = 1;
-            for (let i = 0; i < pageRefs.length; i++){
-                if (pageRefs[i].title === "Page " + latestPage) {
-                    latestPage++;
-                    i = -1;
-                }
-            }
-            title = "Page " + latestPage;
-        }
-
-        setPageRefs(pageRefs.slice(0, pageRefs.length - 1).concat(new PageRef(title, parent.path)));
-        parent.createPage(title);
-        setSelectedPage(title);
-    }
-
-    const deleteNode = (title) => {
-        parent.nodes = parent.nodes.filter((node) => node.title !== title);
-        setNodeRefs(parent.nodeRefGen());
-    }
-
-    return (
-        // TODO when create new node or page, scroll to comfortably view
-        <div id="sidebar" className="h-full w-1/3 overflow-auto border-r-2 border-zinc-500 dark:border-slate-100 select-none">
-            {/* if you want it to stay on top, you'll need to redo your styling with vw units */}
-            <div id="back-button" 
-             onClick={() => { 
-                setSelectedPage(null);
-                if (path.length > 1) setPath(path.slice(0, path.length - 1))
-             }}
-             className="flex items-center p-4 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
-            >
-                <div className="-my-1" dangerouslySetInnerHTML={{__html: (path.length > 1 ? backIcon : homeIcon)}} />
-                <div className='ml-2 text-lg'>{" " + parent.title}</div>
-                
-            </div>
-            
-            <div id="sidebar-btns" className="flex justify-end">
-                <div onClick={newNode} id="new-node-btn" className="px-3 border-2 border-r-0 border-zinc-900 dark:border-slate-100">New Node</div>
-                <div onClick={newPage} id="new-page-btn" className="px-3 border-2 border-zinc-900 dark:border-slate-100">New Page</div>
-            </div>
-
-            <div id="sidebar-list" className='pb-10'>
-                <NodeList setPath={setPath} setSelectedPage={setSelectedPage} nodeRefs={nodeRefs} addNode={addNode} deleteNode={deleteNode} />
-                <hr className={(nodeRefs.length > 0 && pageRefs.length > 0 ? null : "hidden")} />
-                <PageList pageRefs={pageRefs} addPage={addPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} deletePage={deletePage} />
-            </div>
-
+  return (
+    // TODO when create new node or page, scroll to comfortably view
+    <div
+      id="sidebar"
+      className="h-full w-1/3 overflow-auto border-r-2 border-zinc-500 dark:border-slate-100 select-none"
+      onScroll={useXarrow}
+    >
+      {/* if you want it to stay on top, you'll need to redo your styling with vw units */}
+      <div id="sidebar-btns" className="flex justify-end">
+        <div
+          onClick={newNode}
+          id="new-node-btn"
+          className="px-3 border-2 border-r-0 border-zinc-900 dark:border-slate-100"
+        >
+          New Node
         </div>
-    )
+        <div
+          onClick={newPage}
+          id="new-page-btn"
+          className="px-3 border-2 border-zinc-900 dark:border-slate-100"
+        >
+          New Page
+        </div>
+      </div>
+
+      <div
+        id="back-button"
+        onClick={() => {
+          setSelectedPage(null);
+          if (path.length > 1) setPath(path.slice(0, path.length - 1));
+        }}
+        className="flex relative z-10 w-3/4 rounded-2xl m-auto mt-10 items-center p-4 py-5 text-lg bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
+      >
+        <span id="back-beginning" className="-mb-12" />
+        <div
+          id="back-icon"
+          className="-my-1"
+          dangerouslySetInnerHTML={{
+            __html: path.length > 1 ? backIcon : homeIcon,
+          }}
+        />
+        <div className="ml-2 text-lg">{" " + parent.title}</div>
+      </div>
+
+      <div id="sidebar-list" className="pb-10">
+        <NodeList
+          setPath={setPath}
+          setSelectedPage={setSelectedPage}
+          nodeRefs={nodeRefs}
+          addNode={addNode}
+          deleteNode={deleteNode}
+        />
+
+        <hr
+          className={
+            "w-5/6 m-auto " +
+            (nodeRefs.length > 0 && pageRefs.length > 0 ? "" : "hidden")
+          }
+        />
+        <PageList
+          pageRefs={pageRefs}
+          addPage={addPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          deletePage={deletePage}
+        />
+      </div>
+      
+    </div>
+  );
 }
